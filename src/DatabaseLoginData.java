19f1e632ec929f95c912ef03cbe5e06c8744b330
rasmus.karlsson11 <rasmus.karlsson@elev.ga.ntig.se>
2020-03-17 10:21:04 +0100

    Initial commit

---
+++ src/DatabaseLoginData.java
@@ -0,0 +1,8 @@
+public class DatabaseLoginData {
+
+    public static final String DBURL = "localhost";
+    public static final String DBname = "soloadventure";
+    public static String port = "3306";
+    public static String user = "rasmus";
+    public static String password = "password";
+}
